7d2d3d3084e291de766b2501d17b22b201dfecaf
Ali <alisayar0107@gmail.com>
2020-05-19 13:56:06 +0300

    first component created and tachyons added

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Hello.css';
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <div className="f1 tc">
+        <h1>Hello World</h1>
+        <p>Welcome to React</p>
+      </div>
+    );
+  }
+}
+
+export default Hello;
